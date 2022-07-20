aae1274f6c3ac9138d99675f872f1678c8a15670
ajin <aj5210@naver.com>
2022-07-20 21:04:31 +0900

    add security part

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { User } = require('./models/User');
+
+const config = require('./config/key');
+
+app.use(express.json());
+app.use(express.urlencoded( {extended : false } ));
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ajin:961109qw@node-react.jpssu.mongodb.net/?retryWrites=true&w=majority', {   
+mongoose.connect(config.mongoURI, {   
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('수정'))
+
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+      if(err) return res.json({ success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
