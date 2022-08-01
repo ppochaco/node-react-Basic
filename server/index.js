6a7c0c09ab32b1852ea1dc59e108e4e562764df1
ajin <aj5210@naver.com>
2022-08-01 13:08:21 +0900

    modify file structure

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
@@ -14,24 +13,24 @@ app.use(cookieParser());
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {   
 }).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+.catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('수정'))
+app.get('/', (req, res) => res.send('ㅎㅇㅎㅇ'))
 
 
 app.post('/api/users/register', (req, res) => {
-    const user = new User(req.body)
-
-    user.save((err, userInfo) => {
-      if(err) return res.json({ success: false, err})
-      return res.status(200).json({
-        success: true
-      })
+  const user = new User(req.body)
+  
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
     })
+  })
 })
 
 app.post('/api/users/login', (req, res) => {
-
+  
   //1. 요청된 Email을 데이터베이스에서 있는지 찾는다
   User.findOne({ email: req.body.email }, (err, user) => {
     if(!user) {
@@ -44,43 +43,48 @@ app.post('/api/users/login', (req, res) => {
     //2. 요청된 Email이 데이터베이스에 있다면 비밀번호가 맞는지 확인
     user.comparePassword(req.body.password, (err, isMatch) => {
       if(!isMatch)
-        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
-
+      return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+      
       //3. 비밀번호 일치하면 토큰 생성
       user.generateToken((err, user) => {
         if(err) return res.status(400).send(err);
-
+        
         // 토큰을 쿠키에 저장 (로컬스토리지도 가능)
         res.cookie("x_auth", user.token)
-          .status(200)
-          .json({ loginSuccess: true, userId: user._id})
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id})
       })
-  
+      
     })
-
+    
   })
 })
 
 app.get('/api/users/auth', auth, (req, res) => {
   res.status(200).json({
-      _id: req.user._id,
-      isAdmin: req.user.role === 0 ? false : true, //0: 일반, 123..: 관리자
-      isAuth: true,
-      email: req.user.email,
-      name: req.user.name,
-      lastname: req.user.lastname,
-      role: req.user.role,
-      image: req.user.image
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, //0: 일반, 123..: 관리자
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
   })
 })
 
 app.get('/api/users/logout', auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id}, {token: ""}, (err, user) => {
-      if(err) return res.json({ success: false, err});
-      return res.status(200).send({
-        success: true
-      })
+  User.findOneAndUpdate({ _id: req.user._id}, { token: "" }, (err, user) => {
+    if(err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
     })
+  })
 })
+
+
+
+
+const port = 5000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
